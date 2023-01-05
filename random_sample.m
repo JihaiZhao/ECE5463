05ea70d6fad8225a000bcfa3da5a63b329ab9911
JihaiZhao <99274626+JihaiZhao@users.noreply.github.com>
2023-01-04 21:05:50 -0500

    Delete random_sample.m

--- random_sample.m
+++
@@ -1,8 +0,0 @@
-function [xRand, yRand] = random_sample(xMin,yMin,xMax,yMax,bias)
-    if nargin < 5
-        bias = 1;
-    end
-    
-    xRand = (xMax-xMin)*rand^bias;
-    yRand = (yMax-yMin)*rand^bias;
-end
